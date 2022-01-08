d3450b1a5ba7ebd7c3b6bcf5affda0975441cee2
ponty <ponty@home>
2022-01-08 18:28:52 +0100

    test: fix random image differrences

--- vfb.py
+++ vfb.py
@@ -1,12 +1,15 @@
 import os
 import sys
 import threading
+import time
 from pathlib import Path
+from tempfile import TemporaryDirectory
 from time import sleep
 
 import fabric
 import vagrant
 from entrypoint2 import entrypoint
+from PIL import Image, ImageChops
 
 # from fabric.api import env, execute, task, run, sudo, settings
 from vncdotool import api
@@ -30,7 +33,6 @@ def set_resolution(conn, *res):
 
 def run_in_background(conn, cmd):
     # https://docs.fabfile.org/en/1.3.8/faq.html#why-can-t-i-run-programs-in-the-background-with-it-makes-fabric-hang
-    # conn.sudo(f"nohup {cmd} >& /dev/null < /dev/null &", pty=False)
 
     def thread_function(a):
         conn.sudo(cmd, pty=False, warn=True)
@@ -41,10 +43,8 @@ def run_in_background(conn, cmd):
 
 def start_server(conn, rotation, res):
     conn.sudo("killall framebuffer-vncserver", warn=True)
-    # command = f"nohup /home/vagrant/buildc/framebuffer-vncserver -r {rotation}  &> /dev/null &"
     command = f"/home/vagrant/buildc/framebuffer-vncserver -r {rotation}"
     print(f"command: {command}")
-    # conn.sudo(command, pty=False)
     run_in_background(conn, command)
 
 
@@ -52,11 +52,48 @@ def start_server_x11vnc(conn, rotation, res):
     (w, h, depth) = res
     conn.sudo("killall framebuffer-vncserver", warn=True)
     conn.sudo("killall x11vnc", warn=True)
-    # command = f"nohup /usr/bin/x11vnc  -shared -forever -rawfb map:/dev/fb0@{w}x{h}x{depth}  &"
     command = f"/usr/bin/x11vnc  -shared -forever -rawfb map:/dev/fb0@{w}x{h}x{depth}"
     print(f"command: {command}")
-    # conn.sudo(command, pty=False)
     run_in_background(conn, command)
+
+
+def cls(conn):
+    conn.sudo("dd if=/dev/zero of=/dev/fb0", warn=True)
+
+
+def grab():
+    with TemporaryDirectory() as tmpdirname:
+        f = Path(tmpdirname) / "grab.png"
+        with api.connect("localhost:0") as client:
+            client.timeout = 5
+            client.captureScreen(f)
+        im = Image.open(f)
+        return im
+
+
+def _grab_and_sleep(blink_time):
+    start = time.time()
+    im = grab()
+    dt = time.time() - start
+    t = blink_time / 4.0 - dt
+    if t > 0:
+        sleep(t)
+    return im
+
+
+def img_list_min(ls):
+    immin = ls[0]
+    for im in ls[1:]:
+        # ImageChops.subtract= max((a-b),0)
+        diff = ImageChops.subtract(immin, im)
+        immin = ImageChops.subtract(immin, diff)
+    return immin
+
+
+def grab_no_blink(blink_time=1.2):
+    lsim = [_grab_and_sleep(blink_time) for _ in range(4)]
+    im = img_list_min(lsim)
+    return im
 
 
 def shot(conn, directory, png, rotation, *res):
@@ -64,30 +101,24 @@ def shot(conn, directory, png, rotation, *res):
 
     set_resolution(conn, *res)
     start_server(conn, rotation, res)
-    sleep(1.2)
 
     if depth != 8:
-        conn.sudo("killall qmlscene", warn=True)
-        # conn.sudo("qmlscene -platform linuxfb  &> /dev/null &", pty=False)
+        cls(conn)
         run_in_background(conn, "qmlscene -platform linuxfb")
-        sleep(0.2)
-        with api.connect("localhost:0") as client:
-            client.timeout = 5
-            client.captureScreen(directory + "qmlscene_" + png)
-        conn.sudo("killall qmlscene", warn=True)
+        sleep(0.5)
+        grab_no_blink().save(directory + "qmlscene_" + png)
+        conn.sudo("killall qmlscene")
 
     if depth > 1:
+        cls(conn)
         conn.sudo("fb-test")
-        with api.connect("localhost:0") as client:
-            client.timeout = 5
-            client.captureScreen(directory + "fbtest_" + png)
+        grab().save(directory + "fbtest_" + png)
 
+        cls(conn)
         conn.sudo(
             f"python3 /vagrant/gradient.py --width {w} --height {h} --colorbit {depth}"
         )
-        with api.connect("localhost:0") as client:
-            client.timeout = 5
-            client.captureScreen(directory + "gradient_" + png)
+        grab().save(directory + "gradient_" + png)
 
 
 IMGDIR = "img/"
@@ -116,12 +147,13 @@ def main():
         os.makedirs(IMGDIR, exist_ok=True)
         for f in Path(IMGDIR).glob("*.png"):
             f.unlink()
+        conn.sudo("killall qmlscene", warn=True)
 
-        for rot in [90, 180, 270]:
-            tshot(conn, rot, w, h, 16)
         for rot in [0]:
             tshot(conn, rot, w, h, 1)
             tshot(conn, rot, w, h, 8)
             tshot(conn, rot, w, h, 16)
             tshot(conn, rot, w, h, 24)
             tshot(conn, rot, w, h, 32)
+        for rot in [90, 180, 270]:
+            tshot(conn, rot, w, h, 16)
