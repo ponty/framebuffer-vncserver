eeb8b6c7838da8f86b758deafa73572d7056ac85
ponty <ponty@home>
2022-01-10 17:30:42 +0100

    test on ubuntu 22.04

--- Vagrantfile.20.04.rb
+++ Vagrantfile.20.04.rb
@@ -1,10 +1,10 @@
 Vagrant.configure(2) do |config|
-  config.vm.box = "ubuntu/jammy64"
+  config.vm.box = "ubuntu/focal64"
 
   config.vm.network "forwarded_port", guest: 5900, host: 5900
 
   config.vm.provider "virtualbox" do |vb|
-    vb.name = "framebuffer-vncserver.22.04"
+    vb.name = "framebuffer-vncserver.20.04"
     #   vb.gui = true
     #   vb.memory = "1024"
 
@@ -13,11 +13,9 @@ Vagrant.configure(2) do |config|
     # vb.customize ["modifyvm", :id, "--uartmode1", "file", "./ttyS0.log"]
   end
 
-  # disable vbox fb
-  config.vm.provision "shell", inline: "echo 'blacklist vboxvideo' >> /etc/modprobe.d/vbox.conf;sudo update-initramfs -u"
-	# vagrant plugin install vagrant-reload
-  config.vm.provision :reload
   config.vm.provision "shell", path: "vagrant.sh"
 
   config.ssh.extra_args = ["-t", "cd /vagrant;sudo su"]
 end
+
+# export VAGRANT_VAGRANTFILE=Vagrantfile.20.04.rb;export VAGRANT_DOTFILE_PATH=.vagrant_${VAGRANT_VAGRANTFILE}
